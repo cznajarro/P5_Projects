49f9b12c4a76f8a3e333a8a43654b791ae9ddb73
Carlos Zuniga <carlos.zuniganajaarro@outlook.com>
2025-08-02 18:47:40 -0700

    Added everything

---
+++ script.js
@@ -0,0 +1,19 @@
+w = 1200
+h = 800
+
+function setup() {
+  createCanvas(w, h);
+  background(0);
+}
+
+function draw() {
+  let i = random(0,255);
+  let x = random(0,255);
+  let g = random(0, 255);
+  let c = color(i,x,g);
+  let a = random(0,w);
+  let b = random(0,h);
+  noStroke();
+  fill(c);
+  circle(a, b, 20);
+}
