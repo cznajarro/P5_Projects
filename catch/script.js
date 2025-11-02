0e7914afb25ceea80e5a486e687198c68375c2d7
Carlos Zuniga <carlos.zuniganajaarro@outlook.com>
2025-11-01 18:21:13 -0700

    added a pause function when the spacebar is pressed

--- catch/script.js
+++ catch/script.js
@@ -2,6 +2,11 @@ let circleX;
 let circleY;
 let score;
 let speed;
+let b_chance;
+let increase;
+let paused = false;
+let beforespeed;
+increase = 1;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
@@ -16,22 +21,46 @@ function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
 }
 
+function keyPressed(){
+  if (keyCode === 32 && !paused) {
+    paused = true;
+    beforespeed = speed;
+    speed = 0;
+  }
+  else if (keyCode === 32) {
+    speed = beforespeed;
+    paused = false
+  }
+}
+
 function draw() {
   background(220);
   square(mouseX, 740,50 );
   circle(circleX, circleY, 10);
   circleY+=speed;
-  if (circleX < mouseX +20 && circleX > mouseX - 20 && circleY < 780 && circleY > 730) {
-    score +=1
+
+  if (circleX < mouseX +20 && circleX > mouseX - 20 && circleY < 780 && circleY > 730) { //if the circle hits the mouse, increase the score
+    score += increase;
+    let b_chance = random(1,100);
+    if (b_chance <= 25) {
+      fill(0,0,255);
+      increase = 3;
+    }
+    else {
+      increase = 1;
+      fill(255);
+    }
     circleY=0;
     circleX = random(width);
     speed = random(2,5);
   }
-  if (circleY >= 820) {
+
+  if (circleY >= 820) { //if it falls to the bottom, score is set to 0
     score = 0;
     circleY=0;
     circleX = random(width);
     speed = random(2,5);
+    increase = 1;
   }
   text("Score: " + score,10, windowHeight - 20);
 }
